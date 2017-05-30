6f8895a5c5657666425a8a190b440dff57b773e1
ninghaoyu <evil_knight@163.com>
2017-05-30 08:46:52 +0800

    修改

--- getUSshareslist.py
+++ getUSshareslist.py
@@ -4,7 +4,7 @@ import urllib,urllib2,re,cookielib
 from bs4 import BeautifulSoup
 
 PAGES=139
-BASEURL='view-source:https://xueqiu.com/S/?page=%s&exchange=US'
+BASEURL='https://xueqiu.com/S/?page=%s&exchange=US'
 
 
 
