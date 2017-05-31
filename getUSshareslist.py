a75d124dc2f920a0dd38d71c7b5f695c8e6351a1
ninghaoyu <evil_knight@163.com>
2017-05-31 19:37:57 +0800

    修改

--- getUSshareslist.py
+++ getUSshareslist.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
-import urllib,urllib2,re,cookielib
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+import socket,re,os
+import urllib.request
+from pyquery import PyQuery as pq
 
+req_headers = {
+	'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+	}
 PAGES=139
 BASEURL='https://xueqiu.com/S/?page=%s&exchange=US'
+URLS=[ BASEURL % i for i in range(1,PAGES+1) ]
 
+print(URLS)
+
+os._exit(0)
+
+reqobj = urllib.request.Request(BASEURL,headers=req_headers)
+
+req = urllib.request.urlopen(reqobj)
+htm_script=pq(req.read().decode('utf-8'))('script')
+
+print(htm_script.text())
 
 
